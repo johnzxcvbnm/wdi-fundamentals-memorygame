88394b48db2222cd0851bc6204dbf7292f85d01c
Kim Shrum <kimshrum@Kims-Air.home>
2018-05-20 19:39:25 -0500

    created game logic using objects

--- js/main.js
+++ js/main.js
@@ -1,16 +1,39 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if(cardsInPlay.length === 2) {
-		if(cardsInPlay[0] === cardsInPlay[1]) { console.log("Its a match!"); }
-		else { console.log("It's not a match."); }
+		if(cardsInPlay[0] === cardsInPlay[1]) { alert("Its a match!"); }
+		else { alert("It's not a match."); }
 	}
 };
 
 var flipCard = function(cardID) {
-	cardsInPlay.push(cards[cardID]);
-	console.log("User flipped a " + cards[cardID]);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log("User flipped a " + cards[cardID].rank);
+	console.log(cards[cardID].suit);
+	console.log(cards[cardID].cardImage);
 	checkForMatch();
 };
 
