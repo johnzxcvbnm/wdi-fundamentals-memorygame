0a33a09cd38f4e410ece8a0a8591ed5762d29779
Kim Shrum <kimshrum@Kims-Air.home>
2018-05-21 21:26:59 -0500

    added DOM game logic

--- js/main.js
+++ js/main.js
@@ -23,23 +23,32 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-	if(cardsInPlay.length === 2) {
-		if(cardsInPlay[0] === cardsInPlay[1]) { alert("Its a match!"); }
+	if(cardsInPlay[0] === cardsInPlay[1]) { alert("Its a match!"); }
 		else { alert("It's not a match."); }
+};
+
+var flipCard = function() {
+	var cardId = this.getAttribute('id');
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if(cardsInPlay.length === 2)
+	{
+		checkForMatch();
 	}
 };
 
-var flipCard = function(cardID) {
-	cardsInPlay.push(cards[cardID].rank);
-	console.log("User flipped a " + cards[cardID].rank);
-	console.log(cards[cardID].suit);
-	console.log(cards[cardID].cardImage);
-	checkForMatch();
+var createBoard = function() {
+	for(var i = 0; i < cards.length; i++) {
+		var getBoard = document.getElementById("game-board");
+		var cardElement = document.createElement('img');
+		//cardElemnt.setAttribute('src', '/images/back.png');
+		cardElement.src = "images/back.png";
+		cardElement.setAttribute('id', i);
+		cardElement.addEventListener('click', flipCard);
+		getBoard.appendChild(cardElement);
+	}
 };
 
-flipCard(0);
-flipCard(2);
-
-
+createBoard();
 
 
